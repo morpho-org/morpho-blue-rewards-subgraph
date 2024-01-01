3f90b46a2a84dc97b357634ed9d765a6bf17e9b1
julien <julien@morpho.xyz>
2024-01-01 13:38:05 +0100

    fix: overflow issue

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 import { BigInt } from "@graphprotocol/graph-ts";
 
-export const ONE_YEAR = BigInt.fromI32(365 * 24 * 60 * 60);
+export const ONE_YEAR = BigInt.fromString("31536000");
 export const WAD = BigInt.fromI32(1e18 as i32);
